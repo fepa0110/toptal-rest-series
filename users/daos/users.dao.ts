e8d627ea69f91edab8005b0c190cc98a8deaa231
Fabricio <fepafumi97@gmail.com>
2023-06-08 14:01:02 -0300

    feat: :construction: Adding Mongoose

--- users/daos/users.dao.ts
+++ users/daos/users.dao.ts
@@ -1,10 +1,12 @@
-import shortid from 'shortid';
-import debug from 'debug';
-import { CreateUserDto } from '../dto/create.user.dto';
-import { PatchUserDto } from '../dto/patch.user.dto';
-import { PutUserDto } from '../dto/put.user.dto';
+import shortid from "shortid";
+import debug from "debug";
+import { CreateUserDto } from "../dto/create.user.dto";
+import { PatchUserDto } from "../dto/patch.user.dto";
+import { PutUserDto } from "../dto/put.user.dto";
 
-const log: debug.IDebugger = debug('app:in-memory-dao');
+import mongooseService from "../../common/services/mongoose.service";
+
+const log: debug.IDebugger = debug("app:in-memory-dao");
 
 /**
  * NEVER USER THIS CLASS IN REAL LIFE.
@@ -12,74 +14,26 @@ const log: debug.IDebugger = debug('app:in-memory-dao');
  * For any real-life scenario, consider using an ODM/ORM to manage your own database in a better way.
  */
 class UsersDao {
-    users: Array<CreateUserDto> = [];
+	users: Array<CreateUserDto> = [];
+	Schema = mongooseService.getMongoose().Schema;
 
-    constructor() {
-        log('Created new instance of UsersDao');
-    }
+	constructor() {
+		log("Created new instance of UsersDao");
+	}
 
-    async addUser(user: CreateUserDto) {
-        user.id = shortid.generate();
-        this.users.push(user);
-        return user.id;
-    }
+	userSchema = new this.Schema(
+		{
+			_id: String,
+			email: String,
+			password: { type: String, select: false },
+			firstName: String,
+			lastName: String,
+			permissionFlags: Number,
+		},
+		{ id: false }
+	);
 
-    async getUsers() {
-        return this.users;
-    }
-
-    async getUserById(userId: string) {
-        return this.users.find((user: { id: string }) => user.id === userId);
-    }
-
-    async putUserById(userId: string, user: PutUserDto) {
-        const objIndex = this.users.findIndex(
-            (obj: { id: string }) => obj.id === userId
-        );
-        this.users.splice(objIndex, 1, user);
-        return `${user.id} updated via put`;
-    }
-
-    async patchUserById(userId: string, user: PatchUserDto) {
-        const objIndex = this.users.findIndex(
-            (obj: { id: string }) => obj.id === userId
-        );
-        let currentUser = this.users[objIndex];
-        const allowedPatchFields = [
-            'password',
-            'firstName',
-            'lastName',
-            'permissionLevel'
-        ];
-        for (let field of allowedPatchFields) {
-            if (field in user) {
-                // @ts-ignore
-                currentUser[field] = user[field];
-            }
-        }
-        this.users.splice(objIndex, 1, currentUser);
-        return `${user.id} patched`;
-    }
-
-    async removeUserById(userId: string) {
-        const objIndex = this.users.findIndex(
-            (obj: { id: string }) => obj.id === userId
-        );
-        this.users.splice(objIndex, 1);
-        return `${userId} removed`;
-    }
-
-    async getUserByEmail(email: string) {
-        const objIndex = this.users.findIndex(
-            (obj: { email: string }) => obj.email === email
-        );
-        let currentUser = this.users[objIndex];
-        if (currentUser) {
-            return currentUser;
-        } else {
-            return null;
-        }
-    }
+	User = mongooseService.getMongoose().model("Users", this.userSchema);
 }
 
 export default new UsersDao();
